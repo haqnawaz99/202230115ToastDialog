8011fdb8342f0dcca2a586195d03a2e131bed159
haqnawaz <haqnawaz99@yahoo.com>
2023-01-16 12:54:11 +0500

    Multiple choice added

--- app/src/main/java/haqnawaz/org/dialog/MainActivity.java
+++ app/src/main/java/haqnawaz/org/dialog/MainActivity.java
@@ -15,8 +15,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    Button button, buttonAlert, buttonAlert1, buttonAlertList;
+    Button button, buttonAlert, buttonAlert1, buttonAlertList, buttonMC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,25 +28,27 @@ public class MainActivity extends AppCompatActivity {
         buttonAlert = findViewById(R.id.button_alert_dialog);
         buttonAlert1 = findViewById(R.id.button_alert_dialog1);
         buttonAlertList = findViewById(R.id.button_alert_dialog_with_list);
+        buttonMC = findViewById(R.id.button_alert_dialog_with_mc);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Toast.makeText(MainActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "Clicked!", Toast.LENGTH_LONG).show();
                 LayoutInflater inflater = getLayoutInflater();
-                View layout = inflater.inflate(R.layout.my_layout, (ViewGroup) findViewById(R.id.my_toast_viewgroup));
+                View layout = inflater.inflate(R.layout.my_layout,
+                        (ViewGroup) findViewById(R.id.my_toast_viewgroup));
 
-//                TextView text = layout.findViewById(R.id.textView);
-//                text.setText("Customized Toast");
+                TextView text = layout.findViewById(R.id.textView);
+                text.setText("Customized Toast");
 //
 //                ImageView image = layout.findViewById(R.id.imageView);
 //                image.setImageResource(R.drawable.ic_baseline_3p_24);
 
-//                Toast toast = new Toast(MainActivity.this);
-//                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 350);
-//                toast.setDuration(Toast.LENGTH_LONG);
-//                toast.setView(layout);
-//                toast.show();
+                Toast toast = new Toast(MainActivity.this);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 350);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+                toast.show();
             }
         });
 
@@ -54,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setMessage("It is message");
                 builder.setTitle("Title");
-                builder.setCancelable(false);
+                builder.setCancelable(true);
                 builder.setPositiveButton("Positive Button",
                         new DialogInterface.OnClickListener() {
                             @Override
@@ -86,6 +90,47 @@ public class MainActivity extends AppCompatActivity {
                         });
                 AlertDialog dialog = builder.create();
                 dialog.show();
+            }
+        });
+
+        buttonMC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String [] Colors = {"Blue", "Red", "Green"};
+                ArrayList<Integer> selectedItems = new ArrayList();  // Where we track the selected items
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Select options")
+                        .setMultiChoiceItems(Colors, null,
+                                new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which, boolean isChecked)
+                            { if (isChecked) {
+                                selectedItems.add(which);
+                            } else if (selectedItems.contains(which)) {
+                                selectedItems.remove(Integer.valueOf(which));
+                            }
+                            }
+                        })
+                        .setCancelable(false);
+                                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        String msg = "";
+                                        for (int i = 0; i < selectedItems.size(); i++)
+                                        {
+                                            msg = msg + "\n" + (i + 1) + " : " + Colors[ selectedItems.get(i)];
+                                        }
+                                        Toast.makeText(getApplicationContext(), "Total " + selectedItems.size() + " Items Selected.\n" + msg, Toast.LENGTH_SHORT).show();
+                                    }                 })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this,"No Option Selected",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                                AlertDialog dialog  = builder.create();
+                                dialog.show();
             }
         });
     }
